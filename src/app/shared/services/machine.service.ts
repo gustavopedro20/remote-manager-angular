d337d82e6357f6b6790691952028e97070ba9b99
Gustavo <gustavo.goncalves@fncit.com.br>
2020-05-18 19:10:55 -0300

    update url machines

--- src/app/shared/services/machine.service.ts
+++ src/app/shared/services/machine.service.ts
@@ -4,12 +4,15 @@ import { Observable } from 'rxjs';
 
 import { IMachine, Machine } from 'src/app/models/machine.model';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MachineService {
 
-  resourceUrl = 'http://localhost:3000/machines';
+  // resourceUrl = 'http://localhost:3000/machines';
+  resourceUrl = environment.API_URL + '/api/machines';
 
   constructor(private http: HttpClient) { }
 
