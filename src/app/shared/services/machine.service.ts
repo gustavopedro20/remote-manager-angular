7de14b7690332833d1babb1830c507daf5eca78b
LuisHenrique <luis.henrique@fncit.com.br>
2020-05-19 15:51:53 -0300

    Create, Delete, Get machines

--- src/app/shared/services/machine.service.ts
+++ src/app/shared/services/machine.service.ts
@@ -9,28 +9,28 @@ import { IMachine, Machine } from 'src/app/models/machine.model';
 })
 export class MachineService {
 
-  resourceUrl = 'http://localhost:3000/machines';
+  resourceUrl = 'http://localhost:5000';
 
   constructor(private http: HttpClient) { }
 
   findAll(): Observable<IMachine[]> {
-    return this.http.get<IMachine[]>(this.resourceUrl);
+    return this.http.get<IMachine[]>(this.resourceUrl + '/api/machines');
   }
 
   find(machineId: number): Observable<IMachine> {
-    return this.http.get(this.resourceUrl + '/' + machineId);
+    return this.http.get(this.resourceUrl + '/api/machines' + machineId);
   }
 
   delete(machineId: number): Observable<IMachine> {
-    return this.http.delete(this.resourceUrl + '/' + machineId);
+    return this.http.delete(this.resourceUrl + '/api/machines/' + machineId);
   }
 
   create(machine: Machine | IMachine): Observable<IMachine> {
-    return this.http.post(this.resourceUrl, machine);
+    return this.http.post(this.resourceUrl + '/api/machines', machine);
   }
 
   update(machine: Machine | IMachine): Observable<IMachine> {
-    return this.http.put(this.resourceUrl + '/' + machine.id, machine);
+    return this.http.put(this.resourceUrl + '/api/machines', machine);
   }
 
 }
